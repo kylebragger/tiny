2b5c3e3b15437f23e42b78eb4abfe319692870e1
kylebragger <kyle.bragger@gmail.com>
2009-10-02 10:26:34 -0400

    first commit

---
+++ tiny.rb
@@ -0,0 +1,61 @@
+# public function tiny($id){
+#     $set = 'Ed5M8ol0fUxNAJTcZYXyFsOvte2Sjmn43I6wBDah1kiKWbqCLpzQ79ugrPRVGH';
+# 
+#     $HexN="";
+#     $id = floor(abs(intval($id)));
+#     $radix = strlen($set);
+#     while (true) {
+#         $R=$id%$radix;
+#         $HexN = $set{$R}.$HexN;
+#         $id=($id-$R)/$radix;
+#         if ($id==0) break;
+#     }
+#     return $HexN;
+# }
+# public function reverseTiny($str){
+#     $set = 'Ed5M8ol0fUxNAJTcZYXyFsOvte2Sjmn43I6wBDah1kiKWbqCLpzQ79ugrPRVGH';
+#     $radix = strlen($set);
+#     $strlen = strlen($str);
+#     $N = 0;
+#     for($i=0;$i<$strlen;$i++){
+#         $N += strpos($set,$str{$i})*pow($radix,($strlen-$i-1));
+#     }
+#     return "{$N}";
+# }
+
+module Tiny
+  TINY_SET = "Ed5M8ol0fUxNAJTcZYXyFsOvte2Sjmn43I6wBDah1kiKWbqCLpzQ79ugrPRVGH"
+  
+  class << self
+    def tiny(id)
+      hex_n = ''
+      id = id.to_i.floor.abs
+      radix = TINY_SET.length
+      while true
+        r = id % radix
+        hex_n = TINY_SET[r,1] + hex_n
+        id = (id - r) / radix
+        break if id == 0
+      end
+      return hex_n
+    end
+
+    def untiny(str)
+      radix = TINY_SET.length
+      strlen = str.length
+      n = 0
+      (0..strlen - 1).each do |i|
+        n += TINY_SET.index(str[i,1]) * (radix ** (strlen - i - 1))
+      end
+      return n.to_s
+    end
+  end
+end
+
+# Test
+puts Tiny::tiny(12345)
+puts Tiny::tiny(64)
+puts Tiny::tiny(1)
+puts Tiny::untiny(Tiny::tiny(12345))
+puts Tiny::untiny(Tiny::tiny(64))
+puts Tiny::untiny(Tiny::tiny(1))
